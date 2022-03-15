0343d2bdac9e9bebb21855ffc78934af16beed5c
Alexander Maxyutenko <a750mr@gmail.com>
2022-03-15 18:46:49 +0300

    new function

--- broot_password.py
+++ broot_password.py
@@ -8,23 +8,20 @@ pass_link = "https://en.wikipedia.org/wiki/List_of_the_most_common_passwords"
 check_cookie = "https://playground.learnqa.ru/ajax/api/check_auth_cookie"
 
 
-def list_password(response, selector):
-    soup = BeautifulSoup(response.text, "html.parser")
-    my_table = soup.select(selector)[1]
+# Парсим таблицу, убираем элементы html и лишние строки. Приводим к cписку и возвращаем
+def list_password():
+    response_pass = requests.get(pass_link)
+    soup = BeautifulSoup(response_pass.text, "html.parser")
+    my_table = soup.select('table.wikitable')[1]
     my_table = re.sub('<[^>]*>', '', str(my_table))
     list_tab = my_table.split("\n")
     new_list = [i for i in list_tab if i != ""]
     return new_list
 
 
-response_pass = requests.get(pass_link)
-pass_list = list_password(response_pass, 'table.wikitable')
-
-
-# print(pass_list)
-
-
-def make_cookie():
+# Запрашиваем куки для каждого пароля. Хэдеры ответа приобразовываем в json, забираем оттуда строчку Set-Cookie.
+# Откидываем лишнюю часть, добавляем в перемнную эти куки и возвращаем.
+def make_cookie(pass_list):
     username = "super_admin"
     list_cookie = []
     for i in pass_list:
@@ -37,10 +34,7 @@ def make_cookie():
     return list_cookie
 
 
-lscokie = make_cookie()
-print(lscokie)
-
-
+# Спилитим куки, получаем только номер и проверяем на авторизацию. Добавляем верные в переменную и ее возвращаем.
 def broot(cookie):
     pass_cookies = []
     for i in cookie:
@@ -51,21 +45,29 @@ def broot(cookie):
     return pass_cookies
 
 
-pass_cookies = broot(lscokie)
-print(pass_cookies)
+def printoutfulltext(indexcook, allcook):
+    # Приведение куки к полному виду, для получения индексов верных ответов в функции ниже
+    cookie_ls = []
+    for i in indexcook:
+        strokecook = f"auth_cookie={i}"
+        cookie_ls.append(strokecook)
 
-cookie_ls = []
-for i in pass_cookies:
-    strokecook = f"auth_cookie={i}"
-    cookie_ls.append(strokecook)
+    # Получение индексов верных паролей
+    lsnumber = []
+    for i in cookie_ls:
+        num = allcook.index(i)
+        lsnumber.append(num)
+    return lsnumber
 
-lsnumber = []
-for i in cookie_ls:
-    num = lscokie.index(i)
-    lsnumber.append(num)
-print(len(lscokie))
-print(len(pass_list))
-print(lsnumber)
 
-for i in lsnumber:
-    print(pass_list[i])
+def main():
+    pass_list = list_password()
+    al = make_cookie(pass_list)
+    numbers_in_cookie = broot(al)
+    allrightcookie = printoutfulltext(numbers_in_cookie, al)
+    for i in allrightcookie:
+        print(f"Пароль: '{pass_list[i]}', куки для авторизации: '{al[i]}'")
+
+
+if __name__ == "__main__":
+    main()
